96884c1677b2c00e05f30472f422f9c83f312efd
PeteGH <peter@grant-hay.eu>
2017-09-07 19:22:00 +0200

    refactored controller into init func

--- app.js
+++ app.js
@@ -40,9 +40,28 @@ var UIController = (function() { /* Because this is a funcion you want to be abl
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() { /* This function now sets up all of the eventListeners, but it's still private!
+  so this setupEventListeners needs to be called somehow - the best way to do this is to make a public
+  initialization function - I'll call it 'init'*/
+    var DOM = UICtrl.getDOMstrings();
 
-  var ctrlAddItem = function() {
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+      // upon click, the income or expese should be listed as such and added to its respective total
+
+    /* What if you want the user to be able to enter the amount by pressing the 'return' key too?
+    Then you'll need a keyPressEvent - so you'll need another event listener
+      see: https://developer.mozilla.org/en-US/docs/web/events - checkout 'keyboard events'*/
+
+    document.addEventListener('keypress', function(event) { /* Here, I've included the argument 'event'
+    which automatically gets passed into the eventhandler by the browser - you have to know which
+    'keyCode' idendifies the button you want to use (the return key in this case) - http://keycodes.atjayjo.com/#*/
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function() { // This gets called when you want to add a new item, but it's still private!
     // 1. Get the user input data
     var input = UICtrl.getInput();
     console.log(input);
@@ -54,22 +73,21 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 5. Display the updated budget on the UI
 
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-    // upon click, the income or expese should be listed as such and added to its respective total
-
-  /* What if you want the user to be able to enter the amount by pressing the 'return' key too?
-  Then you'll need a keyPressEvent - so you'll need another event listener
-    see: https://developer.mozilla.org/en-US/docs/web/events - checkout 'keyboard events'*/
-
-    document.addEventListener('keypress', function(event) { /* Here, I've included the argument 'event'
-    which automatically gets passed into the eventhandler by the browser - you have to know which
-    'keyCode' idendifies the button you want to use (the return key in this case) - http://keycodes.atjayjo.com/#*/
-      if (event.keyCode === 13 || event.which === 13) {
-        ctrlAddItem();
-      }
-
-    });
+  // Because you want the 'init' function to be public, you need to return it in an object,
+  return {
+    init: function() {
+      setupEventListeners();
+      // ctrlAddItem;
+    }
+  };
 
 })(budgetController, UIController);
+
+  /* So, the eventListeners are only going to be set up as soon as you call the 'init'
+  function - so you need to do that outside of the controllers. This is the only line
+  of code I'm going to place on the outside.*/
+  controller.init(); /* Without this line of code, nothing will ever happen because
+  there won't be any eventListeners and without the eventListeners, you can't input
+  data and without data, there is no application*/
